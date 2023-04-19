a5ded86d5cb3d6e312c2a3a487954f4506fa2b1c
kkesha@inbox.ru <kkesha@inbox.ru>
2023-04-19 17:51:07 +0300

    Modified 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5NjMzOA/spectral.js?branch_slug=master&token=87b148cf-7677-459b-8e26-c7aaf2f76561";
+export default { extends: ruleset };
